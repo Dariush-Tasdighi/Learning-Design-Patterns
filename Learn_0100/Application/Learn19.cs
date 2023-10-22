a4017232d65217f044fcb51a33e8f9a85c45c820
Dariush Tasdighi <DariushT@GMail.com>
2023-10-22 17:23:01 +0330

    Version 1.0.0 - Clean Code

--- Learn_0100/Application/Learn19.cs
+++ Learn_0100/Application/Learn19.cs
@@ -1,69 +1,81 @@
 ﻿namespace Learn19;
 
-// هنوز این درس آخر رو وقت نکردم که ویرایش کنم ولی کدهای قبلی کاملا اوکی هستند!
-
 /// <summary>
 /// The Multi-Instanced Singleton By Thread
 /// We want each thread has it's connection.
 /// </summary>
 public sealed class ConnectionManager : object
 {
-	private static readonly System.Collections.Hashtable _instances;
-
-	public static ConnectionManager Instance
-	{
-		get
-		{
-			//if (instances.Contains(System.Threading.Thread.CurrentThread) == false)
-			//{
-			//	instances.Add(System.Threading.Thread.CurrentThread, new ConnectionManager());
-			//}
-
-			//lock (instances)
-			//{
-			//	if (instances.Contains(System.Threading.Thread.CurrentThread) == false)
-			//	{
-			//		instances.Add(System.Threading.Thread.CurrentThread, new ConnectionManager());
-			//	}
-			//}
-
-			//if (instances.Contains(System.Threading.Thread.CurrentThread) == false)
-			//{
-			//	lock (instances)
-			//	{
-			//		if (instances.Contains(System.Threading.Thread.CurrentThread) == false)
-			//		{
-			//			instances.Add(System.Threading.Thread.CurrentThread, new ConnectionManager());
-			//		}
-			//	}
-			//}
-
-			//return ((ConnectionManager)_instances[System.Threading.Thread.CurrentThread]);
-
-			if (_instances.Contains(System.Threading.Thread.CurrentThread.ManagedThreadId) == false)
-			{
-				lock (_instances)
-				{
-					if (_instances.Contains(System.Threading.Thread.CurrentThread.ManagedThreadId) == false)
-					{
-						_instances.Add(System.Threading.Thread.CurrentThread.ManagedThreadId, new ConnectionManager());
-					}
-				}
-			}
-
-			return (ConnectionManager)_instances[System.Threading.Thread.CurrentThread.ManagedThreadId];
-		}
-	}
-
 	static ConnectionManager()
 	{
 		_instances =
 			new System.Collections.Hashtable();
 	}
 
+	private static readonly object _lockObject = new();
+	private static readonly System.Collections.Hashtable _instances;
+	
+	public static ConnectionManager Instance
+	{
+		get
+		{
+			// **************************************************
+			//if (_instances.Contains(key: System.Threading.Thread.CurrentThread) == false)
+			//{
+			//	var instance = new ConnectionManager();
+
+			//	_instances.Add(key: System.Threading.Thread.CurrentThread, instance);
+			//}
+
+			//// روش احمقانه
+			////var result = (ConnectionManager)_instances[System.Threading.Thread.CurrentThread];
+
+			//var result = _instances[System.Threading.Thread.CurrentThread] as ConnectionManager;
+
+			//return result!;
+			////return result;
+			// **************************************************
+
+			// **************************************************
+			//lock (_lockObject)
+			//{
+			//	if (_instances.Contains(key: System.Threading.Thread.CurrentThread) == false)
+			//	{
+			//		var instance = new ConnectionManager();
+
+			//		_instances.Add(key: System.Threading.Thread.CurrentThread, instance);
+			//	}
+			//}
+
+			//var result = _instances[System.Threading.Thread.CurrentThread] as ConnectionManager;
+
+			//return result!;
+			// **************************************************
+
+			// **************************************************
+			if (_instances.Contains(key: System.Threading.Thread.CurrentThread) == false)
+			{
+				lock (_lockObject)
+				{
+					if (_instances.Contains(key: System.Threading.Thread.CurrentThread) == false)
+					{
+						var instance = new ConnectionManager();
+
+						_instances.Add(key: System.Threading.Thread.CurrentThread, instance);
+					}
+				}
+			}
+
+			var result = _instances[System.Threading.Thread.CurrentThread] as ConnectionManager;
+
+			return result!;
+			// **************************************************
+		}
+	}
+
 	private ConnectionManager()
 	{
 	}
 
-	public string ConnectionString { get; set; }
+	public string? ConnectionString { get; set; }
 }
